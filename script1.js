8bd528317fc0c4410e516173500e5164d59e736b
Kosalvireak Khoeun <virakvary@gmail.com>
2022-07-15 13:46:34 +0700

    add files

---
+++ script1.js
@@ -0,0 +1,89 @@
+
+var utag_data =
+    {
+        "site_region": "us",
+        "page_content_type": "hub",
+        "site_type": "epson:us",
+        "page_type": "category",
+        "cart_total_items": "",
+        "cart_total_value": "0.00",
+        "cart_product_quantity": [],
+        "site_currency": "usd",
+        "site_section": "for_work",
+        "page_hierarchy": "for-work:projectors",
+        "language_code": "en-us",
+        "cart_id": "",
+        "page_category_name": "projectors",
+        "cart_product_price": [],
+        "page_section_name": "for\x2dwork",
+        "customer_group": "guest",
+        "page_name": "epson:us:projectors:hub:w3",
+        "customer_status": "guest",
+        "cart_product_sku": [],
+        "page_subcategory_name": "",
+        "cart_product_id": []
+    }
+        (function (a, b, c, d) {
+            a = '//tags.tiqcdn.com/utag/epson/northamerica/prod/utag.js';
+            b = document; c = 'script'; d = b.createElement(c); d.src = a; d.type = 'text/java' + c; d.async = true;
+            a = b.getElementsByTagName(c)[0]; a.parentNode.insertBefore(d, a);
+        })();
+        var ACC = { config: {} };
+		ACC.config.site_type = 'Epson America';
+		ACC.config.contextPath = "";
+		ACC.config.encodedContextPath = "";
+		ACC.config.commonResourcePath = "/_ui/_v2206091506/responsive/common";
+		ACC.config.themeResourcePath = "/_ui/_v2206091506/responsive/theme-blue";
+		ACC.config.siteResourcePath = "/_ui/_v2206091506/responsive/site-b2c-us";
+		ACC.config.rootPath = "/_ui/_v2206091506/responsive";
+		ACC.config.CSRFToken = "a685a88f-c37c-4106-bab1-8ad48d99b88a";
+		ACC.pwdStrengthVeryWeak = 'Very weak';
+		ACC.pwdStrengthWeak = 'Weak';
+		ACC.pwdStrengthMedium = 'Medium';
+		ACC.pwdStrengthStrong = 'Strong';
+		ACC.pwdStrengthVeryStrong = 'Very strong';
+		ACC.pwdStrengthUnsafePwd = 'password.strength.unsafepwd';
+		ACC.pwdStrengthTooShortPwd = 'password.strength.tooshortpwd';
+		ACC.pwdStrengthMinCharText = 'Minimum password length is %d characters. The password should contain a number, both lower and upper case letters and a special character. The allowed special characters are $, ^, &, %, @, +, =';
+		ACC.accessibilityLoading = 'Loading... Please wait...';
+		ACC.accessibilityStoresLoaded = 'Stores loaded';
+		ACC.crosssellhideplan = 'Hide Plans';
+		ACC.crosssellshowplan = 'Show More Plans';
+		ACC.config.googleApiKey = "AIzaSyDwqzu-Uc1qlNcjFNwToPXDqDX4FHSN4dE";
+		ACC.config.googleApiVersion = "3.7";
+		ACC.config.cartTotalPrice = '';
+
+		ACC.autocompleteUrl = '/search/autocompleteSecure';
+		ACC.config.siteId = "b2c-us";
+		ACC.config.serialNumMaxRows = "10";
+		ACC.config.serialNumMaxLength = "18";
+		ACC.fleetRegSerialNumFileTypeError = 'Imported file must be an Excel file (.xlsx)';
+		ACC.fleetRegSerialNumFileSizeError = 'File too big, select something smaller than';
+		ACC.config.kalungaTrack = "?utm_source={page_content_type}&utm_medium=epson-corporate&utm_content={product_category}-{product_sku}&utm_campaign=br-loja-epson";
+		ACC.config.pdfSpecClassficationCode = "Specifications PDF Link";
+
+		ACC.config.loginUrl = '/oidc';
+		ACC.config.mdrEnabled = "";
+
+
+		ACC.config.authenticationStatusUrl = '/authentication/status';
+        
+		/*<![CDATA[*/
+		ACC.addons = {};	//JS holder for addons properties
+
+
+		ACC.addons.paypal62addon = [];
+
+		ACC.addons.kountaddon = [];
+
+		ACC.addons.smarteditaddon = [];
+
+		ACC.addons.xyformsstorefrontcommons = [];
+
+		ACC.addons.tealiumiqaddon = [];
+
+		ACC.addons.assistedservicestorefront = [];
+
+		ACC.addons.assistedservicestorefront['asm.timer.min'] = 'min';
+
+		/*]]>*/
